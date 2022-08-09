b9dd78c8bdfeccd5b62c2194f1a93560a237517b
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2022-08-09 11:17:06 +0200

    fix: Memory data was not written on NX-Global-Account

--- srv/src/monitoring.ts
+++ srv/src/monitoring.ts
@@ -1,5 +1,6 @@
 const cds = require("@sap/cds");
 const { v4: uuidv4 } = require('uuid')
+import log from "cf-nodejs-logging-support";
 // import * as cds from "@sap/cds";
 
 /**
@@ -45,10 +46,11 @@ export class Monitoring {
    * adds a MemoryInfo object to the respective property 
    */
   public async addMemoryInfo(triggerAction: string, memoryUsage: NodeJS.MemoryUsage, timeInSeconds: number) {
+    log.info(`entering addMemoryInfo with memoryUsage: ${JSON.stringify(memoryUsage, null, 2)}, `)
     const monitoringService = await cds.connect.to("MonitoringService");
     const tx = monitoringService.tx();
     try {
-      const { 'app1.db.monitoring.Memorys': Memories } = cds.entities
+      const Memorys = cds.entities['app1.db.monitoring.Memorys'] || cds.entities['Memorys'] ;
       const entries = [{
         rss: memoryUsage.rss,
         heapTotal: memoryUsage.heapTotal,
@@ -58,20 +60,23 @@ export class Monitoring {
         triggerAction: triggerAction,
         elapsedTime: timeInSeconds
       }]
-      const query = INSERT.into(Memories).entries(entries);
+      const query = INSERT.into(Memorys).entries(entries);
 
       await tx.run(query);
       tx.commit();
+      log.info('exiting addMemoryInfo');
       // this.memoryInfo.push(new MemoryInfo(triggerAction, memoryUsage))
       // // don't save more than 50 records
       // if (this.memoryInfo.length > 50) this.memoryInfo.splice(0, 1);
     } catch (ex: any) {
+      log.error(JSON.stringify(ex, null, 2));
       console.log(ex.message)
       tx.rollback(ex);
     }
   }
 
   public async getMemoryInfo(triggerAction?: string) {
+    log.info('entering getMemoryInfo');
     const monitoringService = await cds.connect.to("MonitoringService");
     try {
       // if (triggerAction) {
@@ -79,11 +84,13 @@ export class Monitoring {
       // } else {
       //   return this.memoryInfo
       // }
-      const { 'app1.db.monitoring.Memorys': Memories } = cds.entities
-      const query = SELECT.from(Memories).orderBy('createdAt');
+      const Memorys = cds.entities['app1.db.monitoring.Memorys'] || cds.entities['Memorys'] ;
+      const query = SELECT.from(Memorys).orderBy('createdAt');
 
+      log.info('exiting getMemoryInfo');
       return await monitoringService.run(query);
     } catch (ex: any) {
+      log.error(JSON.stringify(ex, null, 2));
       console.log(ex.message)
     }
   }
